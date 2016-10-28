f9efef72a27184c5ed43002ce3aeaae742a6d9b5
Mike Schreifels <mike@schreifels.org>
2016-10-28 08:23:37 -0700

    Change ElementTypes data structure

--- src/elementTypes.js
+++ src/elementTypes.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const ElementTypes = [
-  'HEADER_1',
-  'HEADER_2',
-  'HEADER_3',
-  'PARAGRAPH',
-  'ORDERED_LIST',
-  'UNORDERED_LIST'
-];
+const ElementTypes = {
+  '_ROOT': new Set(['HEADER_1']),
+  'HEADER_1': new Set(['HEADER_2', 'HEADER_3', 'PARAGRAPH']),
+  'HEADER_2': new Set(['HEADER_3', 'PARAGRAPH']),
+  'HEADER_3': new Set(['PARAGRAPH']),
+  'PARAGRAPH': new Set(['ORDERED_LIST', 'UNORDERED_LIST']),
+  'ORDERED_LIST': new Set([]),
+  'UNORDERED_LIST': new Set([])
+};
 
 module.exports = ElementTypes;
--- test/elementTypesTest.js
+++ test/elementTypesTest.js
@@ -4,13 +4,27 @@ const assert = require('chai').assert;
 const elementTypes = require('../src/elementTypes');
 
 describe('elementTypes', () => {
-  it('is an array of uppercase strings', () => {
-    assert.isArray(elementTypes);
-    assert.isAbove(elementTypes.length, 0);
+  it('is a map containing only uppercase keys', () => {
+    const elementTypeKeys = Object.keys(elementTypes);
 
-    elementTypes.forEach((type) => {
+    assert.isAbove(elementTypeKeys.length, 0);
+    elementTypeKeys.forEach((type) => {
       assert.isString(type);
       assert.strictEqual(type, type.toUpperCase());
+    });
+  });
+
+  it('contains a root element type', () => {
+    assert.isDefined(elementTypes._ROOT);
+  });
+
+  it('defines a set of valid children for each element type', () => {
+    const elementTypeKeys = Object.keys(elementTypes);
+
+    elementTypeKeys.forEach((type) => {
+      elementTypes[type].forEach((childType) => {
+        assert.isDefined(elementTypes[childType]);
+      });
     });
   });
 });
