f82736e1db3ac56f0eafd6efedefcc4cd0df4012
Mike Schreifels <mike@schreifels.org>
2016-10-31 21:03:05 -0700

    Create Random#intInRange

--- src/random.js
+++ src/random.js
@@ -1,9 +1,16 @@
 'use strict';
 
+function intInRange(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function fromArray(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
 module.exports = {
+  intInRange,
   fromArray
 };
--- test/randomTest.js
+++ test/randomTest.js
@@ -4,6 +4,42 @@ const random = require('../src/random');
 const assert = require('chai').assert;
 
 describe('random', () => {
+  describe('#intInRange', () => {
+    it('returns all possible values given enough iterations', (done) => {
+      let receivedValue1 = false;
+      let receivedValue2 = false;
+      let receivedValue3 = false;
+
+      const array = [1, 2, 3];
+
+      function generateRandomValue() {
+        const randomValue = random.intInRange(1, 3);
+
+        switch (randomValue) {
+          case 1:
+            receivedValue1 = true;
+            break;
+          case 2:
+            receivedValue2 = true;
+            break;
+          case 3:
+            receivedValue3 = true;
+            break;
+          default:
+            assert.fail();
+        }
+
+        if (receivedValue1 && receivedValue2 && receivedValue3) {
+          done();
+        } else {
+          setTimeout(generateRandomValue, 0);
+        }
+      }
+
+      generateRandomValue();
+    });
+  });
+
   describe('#fromArray', () => {
     it('returns undefined when given an empty array', () => {
       assert.isUndefined(random.fromArray([]));
@@ -41,78 +77,6 @@ describe('random', () => {
       }
 
       generateRandomValue();
-    });
-  });
-
-  describe('#validElementTypesForNextElement', () => {
-    context('with no previousElementTypes', () => {
-      it('returns the correct element types', () => {
-        assert.deepEqual(random.validElementTypesForNextElement([]),
-                          ['HEADER_1']);
-      });
-    });
-
-    context('with previousElementType of HEADER_1', () => {
-      it('returns the correct element types', () => {
-        assert.deepEqual(random.validElementTypesForNextElement(['HEADER_1']),
-                          ['HEADER_2', 'PARAGRAPH']);
-      });
-    });
-
-    context('with previousElementType of HEADER_2', () => {
-      it('returns the correct element types', () => {
-        assert.deepEqual(random.validElementTypesForNextElement(['HEADER_2']),
-                          ['HEADER_3', 'PARAGRAPH']);
-      });
-    });
-
-    context('with previousElementType of HEADER_3', () => {
-      it('returns the correct element types', () => {
-        assert.deepEqual(random.validElementTypesForNextElement(['HEADER_3']),
-                          ['PARAGRAPH']);
-      });
-    });
-
-    context('with previousElementType of PARAGRAPH', () => {
-      context('with no header ancestors', () => {
-        it('returns the correct element types', () => {
-          assert.deepEqual(random.validElementTypesForNextElement(['PARAGRAPH']),
-                            ['PARAGRAPH', 'ORDERED_LIST', 'UNORDERED_LIST']);
-        });
-      });
-
-      context('with a header great-grandparent', () => {
-        it('returns the correct element types', () => {
-          assert.deepEqual(random.validElementTypesForNextElement(['HEADER_1', 'PARAGRAPH', 'PARAGRAPH']),
-                            ['HEADER_2', 'PARAGRAPH', 'ORDERED_LIST', 'UNORDERED_LIST']);
-          assert.deepEqual(random.validElementTypesForNextElement(['HEADER_1', 'HEADER_2', 'PARAGRAPH', 'PARAGRAPH']),
-                            ['HEADER_2', 'HEADER_3', 'PARAGRAPH', 'ORDERED_LIST', 'UNORDERED_LIST']);
-          assert.deepEqual(random.validElementTypesForNextElement(['HEADER_1', 'HEADER_2', 'HEADER_3', 'PARAGRAPH', 'PARAGRAPH']),
-                            ['HEADER_2', 'HEADER_3', 'PARAGRAPH', 'ORDERED_LIST', 'UNORDERED_LIST']);
-        });
-      });
-    });
-
-    ['ORDERED_LIST', 'UNORDERED_LIST'].forEach((previousElementType) => {
-      context(`with previousElementType of ${previousElementType}`, () => {
-        context('with no header ancestors', () => {
-          it('returns the correct element types', () => {
-            assert.deepEqual(random.validElementTypesForNextElement([previousElementType]),
-                              ['PARAGRAPH']);
-          });
-        });
-
-        context('with a header great-grandparent', () => {
-          it('returns the correct element types', () => {
-            assert.deepEqual(random.validElementTypesForNextElement(['HEADER_1', 'PARAGRAPH', previousElementType]),
-                              ['HEADER_2', 'PARAGRAPH']);
-            assert.deepEqual(random.validElementTypesForNextElement(['HEADER_1', 'HEADER_2', 'PARAGRAPH', previousElementType]),
-                              ['HEADER_2', 'HEADER_3', 'PARAGRAPH']);
-            assert.deepEqual(random.validElementTypesForNextElement(['HEADER_1', 'HEADER_2', 'HEADER_3', 'PARAGRAPH', previousElementType]),
-                              ['HEADER_2', 'HEADER_3', 'PARAGRAPH']);
-          });
-        });
-      });
     });
   });
 });
