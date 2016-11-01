ecebf96034fcc515aad6a1d37a2d9815e2d7781e
Mike Schreifels <mike@schreifels.org>
2016-10-31 20:35:39 -0700

    Move Random#validElementTypesForNextElement to ElementTypes

--- src/elementTypes.js
+++ src/elementTypes.js
@@ -1,13 +1,52 @@
 'use strict';
 
-const ElementTypes = {
-  '_ROOT': Symbol('_ROOT'),
-  'HEADER_1': Symbol('HEADER_1'),
-  'HEADER_2': Symbol('HEADER_2'),
-  'HEADER_3': Symbol('HEADER_3'),
-  'PARAGRAPH': Symbol('PARAGRAPH'),
-  'ORDERED_LIST': Symbol('ORDERED_LIST'),
-  'UNORDERED_LIST': Symbol('UNORDERED_LIST')
-};
+function headerTypesForNextElement(previousElementTypes) {
+  for (let i = previousElementTypes.length - 1; i >= 0; i--) {
+    switch (previousElementTypes[i]) {
+      case 'HEADER_1':
+        return ['HEADER_2'];
+      case 'HEADER_2':
+        return ['HEADER_2', 'HEADER_3'];
+      case 'HEADER_3':
+        return ['HEADER_2', 'HEADER_3'];
+    }
+  }
 
-module.exports = ElementTypes;
+  return [];
+}
+
+function forNextElement(previousElementTypes) {
+  const validElementTypes = [];
+  const previousElementType = previousElementTypes[previousElementTypes.length - 1];
+
+  if (!previousElementType) {
+    validElementTypes.push('HEADER_1');
+  } else {
+    switch (previousElementType) {
+      case 'HEADER_1':
+        validElementTypes.push('HEADER_2', 'PARAGRAPH')
+        break;
+      case 'HEADER_2':
+        validElementTypes.push('HEADER_3', 'PARAGRAPH')
+        break;
+      case 'HEADER_3':
+        validElementTypes.push('PARAGRAPH')
+        break;
+      case 'PARAGRAPH':
+        validElementTypes.push(...headerTypesForNextElement(previousElementTypes));
+        validElementTypes.push('PARAGRAPH', 'ORDERED_LIST', 'UNORDERED_LIST');
+        break;
+      case 'ORDERED_LIST':
+      case 'UNORDERED_LIST':
+        validElementTypes.push(...headerTypesForNextElement(previousElementTypes));
+        validElementTypes.push('PARAGRAPH');
+        break;
+    }
+  }
+
+  return validElementTypes;
+}
+
+module.exports = {
+  forNextElement
+};
--- test/elementTypesTest.js
+++ test/elementTypesTest.js
@@ -4,26 +4,75 @@ const assert = require('chai').assert;
 const elementTypes = require('../src/elementTypes');
 
 describe('elementTypes', () => {
-  it('is a map containing only uppercase keys', () => {
-    const elementTypeKeys = Object.keys(elementTypes);
-
-    assert.isAbove(elementTypeKeys.length, 0);
-    elementTypeKeys.forEach((type) => {
-      assert.isString(type);
-      assert.strictEqual(type, type.toUpperCase());
+  describe('#forNextElement', () => {
+    context('with no previousElementTypes', () => {
+      it('returns the correct element types', () => {
+        assert.deepEqual(elementTypes.forNextElement([]),
+                          ['HEADER_1']);
+      });
     });
-  });
 
-  it('contains a root element type', () => {
-    assert.isDefined(elementTypes._ROOT);
-  });
+    context('with previousElementType of HEADER_1', () => {
+      it('returns the correct element types', () => {
+        assert.deepEqual(elementTypes.forNextElement(['HEADER_1']),
+                          ['HEADER_2', 'PARAGRAPH']);
+      });
+    });
 
-  it('relates each string to a corresponding symbol', () => {
-    const elementTypeKeys = Object.keys(elementTypes);
+    context('with previousElementType of HEADER_2', () => {
+      it('returns the correct element types', () => {
+        assert.deepEqual(elementTypes.forNextElement(['HEADER_2']),
+                          ['HEADER_3', 'PARAGRAPH']);
+      });
+    });
 
-    elementTypeKeys.forEach((type) => {
-      assert.typeOf(elementTypes[type], 'symbol');
-      assert.strictEqual(elementTypes[type].toString(), `Symbol(${type})`);
+    context('with previousElementType of HEADER_3', () => {
+      it('returns the correct element types', () => {
+        assert.deepEqual(elementTypes.forNextElement(['HEADER_3']),
+                          ['PARAGRAPH']);
+      });
+    });
+
+    context('with previousElementType of PARAGRAPH', () => {
+      context('with no header ancestors', () => {
+        it('returns the correct element types', () => {
+          assert.deepEqual(elementTypes.forNextElement(['PARAGRAPH']),
+                            ['PARAGRAPH', 'ORDERED_LIST', 'UNORDERED_LIST']);
+        });
+      });
+
+      context('with a header great-grandparent', () => {
+        it('returns the correct element types', () => {
+          assert.deepEqual(elementTypes.forNextElement(['HEADER_1', 'PARAGRAPH', 'PARAGRAPH']),
+                            ['HEADER_2', 'PARAGRAPH', 'ORDERED_LIST', 'UNORDERED_LIST']);
+          assert.deepEqual(elementTypes.forNextElement(['HEADER_1', 'HEADER_2', 'PARAGRAPH', 'PARAGRAPH']),
+                            ['HEADER_2', 'HEADER_3', 'PARAGRAPH', 'ORDERED_LIST', 'UNORDERED_LIST']);
+          assert.deepEqual(elementTypes.forNextElement(['HEADER_1', 'HEADER_2', 'HEADER_3', 'PARAGRAPH', 'PARAGRAPH']),
+                            ['HEADER_2', 'HEADER_3', 'PARAGRAPH', 'ORDERED_LIST', 'UNORDERED_LIST']);
+        });
+      });
+    });
+
+    ['ORDERED_LIST', 'UNORDERED_LIST'].forEach((previousElementType) => {
+      context(`with previousElementType of ${previousElementType}`, () => {
+        context('with no header ancestors', () => {
+          it('returns the correct element types', () => {
+            assert.deepEqual(elementTypes.forNextElement([previousElementType]),
+                              ['PARAGRAPH']);
+          });
+        });
+
+        context('with a header great-grandparent', () => {
+          it('returns the correct element types', () => {
+            assert.deepEqual(elementTypes.forNextElement(['HEADER_1', 'PARAGRAPH', previousElementType]),
+                              ['HEADER_2', 'PARAGRAPH']);
+            assert.deepEqual(elementTypes.forNextElement(['HEADER_1', 'HEADER_2', 'PARAGRAPH', previousElementType]),
+                              ['HEADER_2', 'HEADER_3', 'PARAGRAPH']);
+            assert.deepEqual(elementTypes.forNextElement(['HEADER_1', 'HEADER_2', 'HEADER_3', 'PARAGRAPH', previousElementType]),
+                              ['HEADER_2', 'HEADER_3', 'PARAGRAPH']);
+          });
+        });
+      });
     });
   });
 });
